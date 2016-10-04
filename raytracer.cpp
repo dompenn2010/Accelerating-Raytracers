4e480c6902ab3e50bd8bf0257fb1166862ade76f
Craig Tanis <craig-tanis@utc.edu>
2016-10-04 14:50:06 -0400

    raytracer parallelized a little

--- raytracer.cpp
+++ raytracer.cpp
@@ -130,6 +130,7 @@ float mix(const float &a, const float &b, const float &mix)
 // is the color of the object at the intersection point, otherwise it returns
 // the background color.
 //[/comment]
+
 Vec3f trace(
     const Vec3f &rayorig,
     const Vec3f &raydir,
@@ -221,35 +222,40 @@ Vec3f trace(
 void render(const std::vector<Sphere> &spheres)
 {
     // unsigned width = 640, height = 480;
-    // unsigned width = 4096, height = 2160;
-    unsigned width = 2048, height = 1024;
+    unsigned width = 4096, height = 2160;
+    //unsigned width = 2048, height = 1024;
     Vec3f *image = new Vec3f[width * height], *pixel = image;
     float invWidth = 1 / float(width), invHeight = 1 / float(height);
     float fov = 30, aspectratio = width / float(height);
     float angle = tan(M_PI * 0.5 * fov / 180.);
     // Trace rays
-    for (unsigned y = 0; y < height; ++y) {
+
+    #pragma omp parallel for
+    for (unsigned y = 0; y < height; y++) {
 
         if (y % 10 == 0)
         {
             std::cerr << "rendering " << y << " of " << height << std::endl;
         }
 
-        for (unsigned x = 0; x < width; ++x, ++pixel) {
+        for (unsigned int x = 0; x < width; x++) {
             float xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
             float yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle;
             Vec3f raydir(xx, yy, -1);
             raydir.normalize();
-            *pixel = trace(Vec3f(0), raydir, spheres, 0);
+            // *pixel = trace(Vec3f(0), raydir, spheres, 0);
+
+            image[y*width+x] = trace(Vec3f(0), raydir, spheres, 0);
         }
     }
+
     // Save result to a PPM image (keep these flags if you compile under Windows)
     std::ofstream ofs("./untitled.ppm", std::ios::out | std::ios::binary);
     ofs << "P6\n" << width << " " << height << "\n255\n";
     for (unsigned i = 0; i < width * height; ++i) {
         ofs << (unsigned char)(std::min(float(1), image[i].x) * 255) <<
-               (unsigned char)(std::min(float(1), image[i].y) * 255) <<
-               (unsigned char)(std::min(float(1), image[i].z) * 255);
+            (unsigned char)(std::min(float(1), image[i].y) * 255) <<
+            (unsigned char)(std::min(float(1), image[i].z) * 255);
     }
     ofs.close();
     delete [] image;
@@ -275,7 +281,7 @@ int main(int argc, char **argv)
     // // light
     // spheres.push_back(Sphere(Vec3f( 0.0,     20, -30),     3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(3)));
 
-    static const int NUM_SPHERES=300;
+    static const int NUM_SPHERES=3000;
 
     for (int s=0; s<NUM_SPHERES; s++)
     {
